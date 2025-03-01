b340eb7a5439b5f1854e05ad0703d91dd482946f
Zelectra <varizkynr@gmail.com>
2025-03-01 13:21:17 +0700

    Pembuatan file migrasi dengan relasi

--- Semester_4/Pemrograman Web Lanjut/PWL_POS/database/migrations/2025_02_26_030428_create_m_user_table.php
+++ Semester_4/Pemrograman Web Lanjut/PWL_POS/database/migrations/2025_02_26_030428_create_m_user_table.php
@@ -6,22 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::create('m_kategori', function (Blueprint $table) {
-            $table->id();
+        Schema::create('m_user', function (Blueprint $table) {
+            $table->id('user_id');
+            $table->string('username', 20)->unique();
+            $table->string('nama', 100);
+            $table->string('password', 255);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('m_kategori');
+        Schema::dropIfExists('m_user');
     }
 };
