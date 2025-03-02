39f2103f16557703255cecb3fb5ee041f36e1964
Zelectra <varizkynr@gmail.com>
2025-03-02 11:30:53 +0700

    Prak 6 - Implementasi Eloquent ORM

--- Semester_4/Pemrograman Web Lanjut/PWL_POS/routes/web.php
+++ Semester_4/Pemrograman Web Lanjut/PWL_POS/routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,5 +20,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/level',[LevelController::class,'index']);
+Route::get('/kategori',[KategoriController::class,'index']);
+Route::get('/user',[UserController::class,'index']);
