407d4fb0cafbbbd7691377f612774426d45eda96
Zelectra <varizkynr@gmail.com>
2025-03-05 10:50:24 +0700

    praktikum 2.4

--- Semester_4/Pemrograman Web Lanjut/PWL_POS/app/Http/Controllers/UserController.php
+++ Semester_4/Pemrograman Web Lanjut/PWL_POS/app/Http/Controllers/UserController.php
@@ -26,12 +26,20 @@ class UserController extends Controller
 
         // coba akses model UserModel
 
-        $user = UserModel::where('level_id', 2)->count();
-        // dd($user);
+        // $user = UserModel::where('level_id', 2)->count();
+        // // dd($user);
+        // return view('user', ['data' => $user]);
+
+        $user = UserModel::firstOrCreate([
+            'username' => 'manager33',
+            'nama' => 'Manager tiga tiga',
+            'password' => Hash::make('12345'),
+            'level_id' => 2
+        ],
+    );
+    $user->save();
+    
         return view('user', ['data' => $user]);
-
-
-
 
     }
 }
