4a93b3dbd5d4d74ba572b563e552a2dd380e4ae3
Zelectra <varizkynr@gmail.com>
2025-03-05 09:02:13 +0700

    praktikum 1 - fillable

--- Semester_4/Pemrograman Web Lanjut/PWL_POS/app/Http/Controllers/UserController.php
+++ Semester_4/Pemrograman Web Lanjut/PWL_POS/app/Http/Controllers/UserController.php
@@ -11,18 +11,21 @@ class UserController extends Controller
     public function index(){
         // tambah data user dengan Eloquent Model
         $data = [
-            // 'username' => 'customer-1',
-            // 'nama' => 'Pelanggan',
-            // 'password' => Hash::make('12345'),
-            // 'level_id' => 2
+            'username' => 'manager_tiga',
+            'level_id' => 2,
+            'password' => Hash::make('12345'),
+            'nama' => 'Manager 3'
 
-            'nama'=>'Pelanggan Pertama',
+            // 'nama'=>'Pelanggan Pertama',
         ];
         // UserModel::insert($data); // tambahkan data ke tabel m_user
 
-        UserModel::where('username', 'customer-1')->update($data);
+        UserModel::create($data);
+
+        // UserModel::where('username', 'customer-1')->update($data);
 
         // coba akses model UserModel
+
         $user = UserModel::all(); // ambil semua data dari tabel m_user
         return view('user', ['data' => $user]);
     }
