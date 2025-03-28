7b35b6f2e3e635a8dbc90d491874d40a01eb6bc2
Zelectra <varizkynr@gmail.com>
2025-03-28 18:57:29 +0700

    JS 7 Praktikum 1. Done

---
+++ Semester_4/Pemrograman Web Lanjut/PWL_POS/app/Http/Controllers/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login() {
+        if(auth()->check()) {
+            return redirect('/');
+        }
+        return view('auth.login');
+    }
+    
+    public function postlogin(Request $request) {
+        if($request->ajax() || $request->wantsJson()) {
+            $credentials = $request->only('email', 'password');
+            if (auth::attempt($credentials)) {
+                return response()->json ([
+                    'status' => true,
+                    'message' => 'Login Berhasil',
+                    'redirect' => url('/')
+                ]);
+    }
+    
+    return response()->json([
+        'status' => false,
+        'message' => 'Login Failed'
+    ]);
+}
+return redirect('login');
+}
+
+public function logout(Request $request) 
+{
+    Auth::logout();
+    
+    $request->session()->invalidate();
+    $request->session()->regenerateToken();
+    return redirect('/login');
+}
+}
+
