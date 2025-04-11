ee198ef2d880e81912102115475e7f463f53493f
Zelectra <varizkynr@gmail.com>
2025-04-11 10:05:28 +0700

    Export PDF done, Prak 3, JS 8

--- Semester_4/Pemrograman Web Lanjut/PWL_POS/app/Http/Controllers/BarangController.php
+++ Semester_4/Pemrograman Web Lanjut/PWL_POS/app/Http/Controllers/BarangController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use Yajra\DataTables\Facades\DataTables;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class BarangController extends Controller
 {
@@ -170,7 +171,7 @@ class BarangController extends Controller
     
             $insert = [];
     
-            if (count($data) > 1) { // jika data lebih dari 1 baris
+            if (count($data) > 1) { // jika data lebih dari 1 barisavlo wwwww                       
                 foreach ($data as $baris => $value) {
                     if ($baris > 1) { // baris ke 1 adalah header, maka lewati
                         $insert[] = [
@@ -207,12 +208,14 @@ class BarangController extends Controller
 
 
 
-    public function export()
+    public function export_excel()
     {
+        // ambil data barang
         $barang = BarangModel::select('kategori_id','barang_kode', 'barang_nama','harga_beli','harga_jual')
             ->orderBy('kategori_id')
             ->with('kategori')
             ->get();
+            // Load Library PhpSpreadsheet
         $spreadsheet = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
         $sheet = $spreadsheet->getActiveSheet();
         $sheet->setCellValue('A1', 'No');
@@ -258,4 +261,25 @@ class BarangController extends Controller
         $writer->save('php://output');
         exit;
     }
+
+    public function export_pdf()
+{
+    $barang = BarangModel::select('kategori_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual')
+        ->orderBy('kategori_id')
+        ->orderBy('barang_kode')
+        ->with('kategori')
+        ->get();
+
+    // Gunakan Barryvdh\DomPDF\Facade\Pdf
+    $pdf = Pdf::loadView('barang.export_pdf', [
+        'barang' => $barang
+    ]);
+
+    $pdf->setPaper('a4', 'portrait'); // Set ukuran kertas dan orientasi
+    $pdf->setOption('isRemoteEnabled', true); // Aktifkan jika ada gambar dari URL
+    $pdf->render();
+
+    return $pdf->stream('Data Barang ' . date('Y-m-d H:i:s') . '.pdf');
+}
+
 }
--- Semester_4/Pemrograman Web Lanjut/PWL_POS/resources/views/barang/index.blade.php
+++ Semester_4/Pemrograman Web Lanjut/PWL_POS/resources/views/barang/index.blade.php
@@ -1,120 +1,145 @@
 @extends('layouts.template')
 
 @section('content')
-<div class="card">
-    <div class="card-header ">
-        <h3 class="card-title">Daftar Barang</h3>
-        <div class="card-tools">
-            <button onclick="modalAction('{{ url('/barang/import') }}')" class="btn btn-info">Import Barang</button>
-            <a href="{{ url('/barang/export_excel') }}" class="btn btn-primary"><i class="fa fa-file excel"></i> Export
-                Barang</a>
-          
-            <button onclick="modalAction('{{ url(path: '/barang/create_ajax') }}')" class="btn btn-success">Tambah Ajax</button>
+    <div class="card">
+        <div class="card-header ">
+            <h3 class="card-title">Daftar Barang</h3>
+            <div class="card-tools">
+                <button onclick="modalAction('{{ url('/barang/import') }}')" class="btn btn-info">Import Barang</button>
+                <a href="{{ url('/barang/export_excel') }}" class="btn btn-primary"><i class="fa fa-file excel"></i> Export
+                    Barang</a>
+                <a href="{{ url('/barang/export_pdf') }}" class="btn btn-warning"><i class="fa fa-file-pdf"></i> Export
+                    PDF</a>
+            </div>
         </div>
-    </div>
 
-    <div class="card-body">
-        <!-- Filter Data -->
-        <div id="filter" class="form-horizontal filter-date p-2 border-bottom mb-2">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="form-group form-group-sm row text-sm mb-0">
-                        <label for="filter_date" class="col-md-1 col-form-label">Filter</label>
-                        <div class="col-md-3">
-                            <select name="filter_kategori" class="form-control form-control-sm filter_kategori">
-                                <option value="">- Semua -</option>
-                                @foreach($kategori as $l)
-                                    <option value="{{ $l->kategori_id }}">{{ $l->kategori_nama }}</option>
-                                @endforeach
-                            </select>
-                            <small class="form-text text-muted">Kategori Barang</small>
+        <div class="card-body">
+            <!-- Filter Data -->
+            <div id="filter" class="form-horizontal filter-date p-2 border-bottom mb-2">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="form-group form-group-sm row text-sm mb-0">
+                            <label for="filter_date" class="col-md-1 col-form-label">Filter</label>
+                            <div class="col-md-3">
+                                <select name="filter_kategori" class="form-control form-control-sm filter_kategori">
+                                    <option value="">- Semua -</option>
+                                    @foreach ($kategori as $l)
+                                        <option value="{{ $l->kategori_id }}">{{ $l->kategori_nama }}</option>
+                                    @endforeach
+                                </select>
+                                <small class="form-text text-muted">Kategori Barang</small>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <!-- Alert Success/Error -->
+            @if (session('success'))
+                <div class="alert alert-success">{{ session('success') }}</div>
+            @endif
+            @if (session('error'))
+                <div class="alert alert-danger">{{ session('error') }}</div>
+            @endif
+
+            <!-- Table Barang -->
+            <table class="table table-bordered table-sm table-striped table-hover" id="table-barang">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Kode Barang</th>
+                        <th>Nama Barang</th>
+                        <th>Harga Beli</th>
+                        <th>Harga Jual</th>
+                        <th>Kategori</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            </table>
         </div>
-
-        <!-- Alert Success/Error -->
-        @if(session('success'))
-            <div class="alert alert-success">{{ session('success') }}</div>
-        @endif
-        @if(session('error'))
-            <div class="alert alert-danger">{{ session('error') }}</div>
-        @endif
-
-        <!-- Table Barang -->
-        <table class="table table-bordered table-sm table-striped table-hover" id="table-barang">
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Kode Barang</th>
-                    <th>Nama Barang</th>
-                    <th>Harga Beli</th>
-                    <th>Harga Jual</th>
-                    <th>Kategori</th>
-                    <th>Aksi</th>
-                </tr>
-            </thead>
-            <tbody></tbody>
-        </table>
     </div>
-</div>
 
-<!-- Modal -->
-<div id="myModal" class="modal fade animate shake" tabindex="-1" data-backdrop="static" data-keyboard="false" data-width="75%"></div>
+    <!-- Modal -->
+    <div id="myModal" class="modal fade animate shake" tabindex="-1" data-backdrop="static" data-keyboard="false"
+        data-width="75%"></div>
 @endsection
 
 @push('js')
-<script>
-    function modalAction(url = '') {
-        $('#myModal').load(url, function () {
-            $('#myModal').modal('show');
-        });
-    }
+    <script>
+        function modalAction(url = '') {
+            $('#myModal').load(url, function() {
+                $('#myModal').modal('show');
+            });
+        }
 
-    var tableBarang;
-    $(document).ready(function () {
-        tableBarang = $('#table-barang').DataTable({
-            processing: true,
-            serverSide: true,
-            ajax: {
-                url: "{{ url('barang/list') }}",
-                type: "POST",
-                dataType: "json",
-                data: function (d) {
-                    d.filter_kategori = $('.filter_kategori').val();
+        var tableBarang;
+        $(document).ready(function() {
+            tableBarang = $('#table-barang').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: {
+                    url: "{{ url('barang/list') }}",
+                    type: "POST",
+                    dataType: "json",
+                    data: function(d) {
+                        d.filter_kategori = $('.filter_kategori').val();
+                    }
+                },
+                columns: [{
+                        data: "barang_id",
+                        className: "text-center",
+                        width: "5%",
+                        orderable: false,
+                        searchable: false
+                    },
+                    {
+                        data: "barang_kode",
+                        width: "10%"
+                    },
+                    {
+                        data: "barang_nama",
+                        width: "37%"
+                    },
+                    {
+                        data: "harga_beli",
+                        width: "10%",
+                        render: function(data) {
+                            return new Intl.NumberFormat('id-ID').format(data);
+                        }
+                    },
+                    {
+                        data: "harga_jual",
+                        width: "10%",
+                        render: function(data) {
+                            return new Intl.NumberFormat('id-ID').format(data);
+                        }
+                    },
+                    {
+                        data: "kategori.kategori_nama",
+                        width: "14%"
+                    },
+                    {
+                        data: "aksi",
+                        className: "text-center",
+                        width: "14%",
+                        orderable: false,
+                        searchable: false
+                    }
+                ]
+            });
+
+            // Trigger search on enter
+            $('#table-barang_filter input').unbind().bind('keyup', function(e) {
+                if (e.keyCode == 13) {
+                    tableBarang.search(this.value).draw();
                 }
-            },
-            columns: [
-                { data: "barang_id", className: "text-center", width: "5%", orderable: false, searchable: false },
-                { data: "barang_kode", width: "10%" },
-                { data: "barang_nama", width: "37%" },
-                {
-                    data: "harga_beli", width: "10%", render: function (data) {
-                        return new Intl.NumberFormat('id-ID').format(data);
-                    }
-                },
-                {
-                    data: "harga_jual", width: "10%", render: function (data) {
-                        return new Intl.NumberFormat('id-ID').format(data);
-                    }
-                },
-                { data: "kategori.kategori_nama", width: "14%" },
-                { data: "aksi", className: "text-center", width: "14%", orderable: false, searchable: false }
-            ]
-        });
+            });
 
-        // Trigger search on enter
-        $('#table-barang_filter input').unbind().bind('keyup', function (e) {
-            if (e.keyCode == 13) {
-                tableBarang.search(this.value).draw();
-            }
+            // Filter kategori
+            $('.filter_kategori').change(function() {
+                tableBarang.draw();
+            });
         });
-
-        // Filter kategori
-        $('.filter_kategori').change(function () {
-            tableBarang.draw();
-        });
-    });
-</script>
+    </script>
 @endpush
